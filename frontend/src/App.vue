1ad714d6419ab018e0ec6815bf0bdca90e8ab5ad
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-06-16 05:57:18 +0000

    feat: Home Page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,67 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
+import ServiceCard from '@/components/ServiceCard.vue'
+import { Icon } from '@iconify/vue'
+
+interface Service {
+  src: string,
+  src2: string | undefined,
+  title: string,
+  description: string
+}
+
+const services: Service[] = [
+  {
+    src: 'src/assets/images/haircut-img-comp.webp',
+    src2: undefined,
+    title: 'Haircut',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    src: 'src/assets/images/manicure-img-comp.webp',
+    src2: 'src/assets/images/pedicure-img-comp.webp',
+    title: 'Manicure & Pedicure',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+  {
+    src: 'src/assets/images/facial-img-comp.webp',
+    src2: undefined,
+    title: 'Facial Treatment',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },
+]
 </script>
 
 <template>
-  <Button>Hai</Button>
+  <div class="flex w-screen h-screen justify-center items-center">
+    <div class="bg-white flex flex-col w-[50%] justify-center">
+        <p class="text-8xl text-center">SEA Salon</p>
+        <p class="text-3xl text-center">Beauty and Elegance, Redefined</p>
+    </div>
+    <div class="w-[50%]">
+        <img class="w-full h-screen object-cover" src="@/assets/images/home-page-img-comp.webp" alt="">
+    </div>
+  </div>
+  <div class="my-2">
+    <h1 class="text-center text-4xl mb-3">Our Services</h1>
+    <div class="flex gap-10 px-2 justify-center">
+      <ServiceCard v-for="service in services" :key="service.title" :src="service.src" :src2="service.src2" :title="service.title" :description="service.description"/>
+    </div>
+  </div>
+  <div>
+    <h1 class="text-center text-4xl mb-3">Contact Us</h1>
+    <div class="flex gap-5 justify-center">
+      <div class="">
+        <img src="" alt="" class="rounded-full">
+        <p class="text-center"><Icon icon="mdi:account" class="inline" />Thomas</p>
+        <p class="text-center"><Icon icon="ph:phone-fill" class="inline" />08123456789</p>
+      </div>
+      <div class="">
+        <img src="" alt="">
+        <p class="text-center"><Icon icon="mdi:account" class="inline" />Sekar</p>
+        <p class="text-center"><Icon icon="ph:phone-fill" class="inline" />08164829372</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ frontend/src/components/ServiceCard.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+defineProps<{
+    src: string,
+    src2: string | undefined,
+    title: string,
+    description: string    
+}>()
+</script>
+
+<template>
+    <div class="flex flex-col rounded-xl overflow-hidden w-[360px] shadow-xl">
+        <img v-if="!src2" :src="src" alt="" class="w-full h-96 object-cover">
+        <div v-else class="flex">
+            <img class="w-1/2 h-96 object-cover" :src="src" alt="">
+            <img class="w-1/2 h-96 object-cover" :src="src2" alt="">
+        </div>
+        <div class="pb-3">
+            <!-- Title -->
+            <p class="text-center text-xl mt-2">{{ title }}</p>
+            <!-- Description -->
+            <p class="text-center">{{ description }}</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
