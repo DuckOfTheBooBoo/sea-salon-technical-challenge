39d58952735468694c8a0302192b7f0793880bcc
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-06-16 17:03:44 +0000

    feat: Set default font to Inter

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -21,6 +21,9 @@ module.exports = {
         "2xl": "1400px",
       },
     },
+    fontFamily: {
+      sans: ['"Inter"', 'sans-serif'],
+    },
     extend: {
       colors: {
         border: "hsl(var(--border))",
