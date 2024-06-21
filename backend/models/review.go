b47727a8ad393184a10bdebe74793e5bd0f4dbe2
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-06-21 16:38:32 +0000

    refactor(backend): Change review model JSON field

--- backend/models/review.go
+++ backend/models/review.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Review struct {
 	gorm.Model
-	FullName string `gorm:"not null"`
-	Rating uint `gorm:"not null"`
-	Comment string `gorm:"not null"`
+	FullName string `gorm:"not null" json:"full_name"`
+	Rating uint `gorm:"not null" json:"rating"`
+	Comment string `gorm:"not null" json:"comment"`
 }
