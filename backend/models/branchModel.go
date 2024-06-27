a22433914ac031952bfeb68e53dd8f210ebbc481
Arajdian Altaf <arajdianaltaf123@gmail.com>
2024-06-27 10:42:56 +0000

    feat(backend/model): Add json field

--- backend/models/branchModel.go
+++ backend/models/branchModel.go
@@ -11,7 +11,7 @@ type Branch struct {
 	BranchAddress string     `gorm:"not null" json:"branch_address"`
 	Lat           float64    `gorm:"not null" json:"lat"`
 	Lng           float64    `gorm:"not null" json:"lng"`
-	OpenAt        string  `gorm:"not null;type:time"`
-	ClosedAt      string  `gorm:"not null;type:time"`
-	Services      []Service `gorm:"many2many:branch_services" json:"branch_services"`
+	OpenAt        string  `gorm:"not null;type:time" json:"open_time"`
+	ClosedAt      string  `gorm:"not null;type:time" json:"close_time"`
+	Services      []Service `gorm:"many2many:branch_services" json:"services"`
 }
