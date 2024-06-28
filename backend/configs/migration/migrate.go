bb5c454b3b26d5b94a7bbd46f8d8916903113c6c
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-06-28 12:31:51 +0000

    fix(backend): fix migration error by wrong model order

--- backend/configs/migration/migrate.go
+++ backend/configs/migration/migrate.go
@@ -23,7 +23,7 @@ func main() {
 
 	gormDB := db.GetDB()
 
-	if err := gormDB.AutoMigrate(&models.User{}, &models.Reservation{}, &models.Review{}, &models.Token{}, &models.Branch{}, &models.Service{}); err != nil {
+	if err := gormDB.AutoMigrate(&models.User{}, &models.Branch{}, &models.Reservation{}, &models.Review{}, &models.Token{}, &models.Service{}); err != nil {
 		log.Fatal(err.Error())
 		return
 	}
