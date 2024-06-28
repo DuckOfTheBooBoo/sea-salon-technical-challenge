4b5e35a5741ee17a8daed2290eb39ee35f964efa
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-06-28 04:27:36 +0000

    refactor(backend): Don't include branch_services into response JSON

--- backend/models/serviceModel.go
+++ backend/models/serviceModel.go
@@ -9,5 +9,5 @@ type Service struct {
 	ServiceName string `gorm:"not null;unique" json:"service_name"`
 	ServiceCode string `gorm:"not null;unique" json:"service_code"`
 	Duration uint `gorm:"not null" json:"duration"`
-	Branches []Branch `gorm:"many2many:branch_services" json:"branch_services"`
+	Branches []Branch `gorm:"many2many:branch_services" json:"-"`
 }
