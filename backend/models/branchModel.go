3a8b3a54e2b6d9873b270bddf1ceb9e03bac7286
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-06-28 12:32:31 +0000

    feat(backend/model): Implement custom gorm model for time datatype

--- backend/models/branchModel.go
+++ backend/models/branchModel.go
@@ -1,17 +1,17 @@
 package models
 
 import (
-
 	"gorm.io/gorm"
 )
 
 type Branch struct {
 	gorm.Model
-	BranchName    string     `gorm:"not null" json:"branch_name"`
-	BranchAddress string     `gorm:"not null" json:"branch_address"`
-	Lat           float64    `gorm:"not null" json:"lat"`
-	Lng           float64    `gorm:"not null" json:"lng"`
-	OpenAt        string  `gorm:"not null;type:time" json:"open_time"`
-	ClosedAt      string  `gorm:"not null;type:time" json:"close_time"`
-	Services      []Service `gorm:"many2many:branch_services" json:"services"`
+	BranchName    string        `gorm:"not null" json:"branch_name"`
+	BranchAddress string        `gorm:"not null" json:"branch_address"`
+	Lat           float64       `gorm:"not null" json:"lat"`
+	Lng           float64       `gorm:"not null" json:"lng"`
+	OpenAt        MySQLTime     `gorm:"type:time;not null" json:"open_time"`
+	ClosedAt      MySQLTime     `gorm:"type:time;not null;" json:"close_time"`
+	Services      []Service     `gorm:"many2many:branch_services" json:"services"`
+	Reservations  []Reservation `gorm:"foreignKey:BranchID" json:"-"`
 }
