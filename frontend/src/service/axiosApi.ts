79ba0d517cec7c15b8cdbb8b1b4f704d47d2a90f
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-06-28 04:28:05 +0000

    feat(frontend/auth): Add interceptors to include auth token for protected request

--- frontend/src/service/axiosApi.ts
+++ frontend/src/service/axiosApi.ts
@@ -1,12 +1,21 @@
-import axios from 'axios';
-import { AxiosInstance } from 'axios';
+import axios, { type AxiosInstance } from 'axios';
 
 const apiClient: AxiosInstance = axios.create({
     baseURL: 'http://localhost:3000/api',
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${localStorage.getItem('token')}`,
     },
 })
+
+apiClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers = {
+            ...config.headers,
+            Authorization: `Bearer ${token}`,
+        };
+    }
+    return config;
+}, (error) => Promise.reject(error));
 
 export default apiClient;
