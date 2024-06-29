7a8eaaffd2ac10fc0c730bc6c8e3a5dc4f373242
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-06-29 07:48:34 +0000

    feat(backend): Hardcode admin credentials

--- backend/configs/migration/migrate.go
+++ backend/configs/migration/migrate.go
@@ -6,10 +6,43 @@ import (
 	"github.com/DuckOfTheBooBoo/sea-salon-technical-challenge/backend/configs"
 	"github.com/DuckOfTheBooBoo/sea-salon-technical-challenge/backend/models"
 	"github.com/DuckOfTheBooBoo/sea-salon-technical-challenge/backend/utils"
+	"gorm.io/gorm"
 )
 
 func init() {
 	utils.LoadEnv()
+}
+
+func createAdminUser(gormDB *gorm.DB) {
+	var user models.User
+	if err := gormDB.Where("email = ?", "thomas.n@compfest.id").First(&user).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+
+			hashedPass, err := utils.HashPassword("Admin123")
+
+			if err != nil {
+				log.Fatal(err.Error())
+				return
+			}
+
+			user = models.User{
+				FullName: "Thomas N",
+				Email:    "thomas.n@compfest.id",
+				Password: hashedPass,
+				Role:     "Admin",
+			}
+
+			if err := gormDB.Create(&user); err != nil {
+				log.Fatal(err)
+			}
+
+			log.Print("Admin user created successfully")
+		} else {
+			log.Fatal(err)
+		}
+	} else {
+		log.Print("Admin user already exists")
+	}
 }
 
 func main() {
@@ -28,5 +61,5 @@ func main() {
 		return
 	}
 
-
+	createAdminUser(gormDB)
 }
--- backend/models/userModel.go
+++ backend/models/userModel.go
@@ -4,11 +4,11 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
-	FullName string `gorm:"not null" json:"full_name"`
-	Email string `gorm:"type:varchar(255);not null;unique" json:"email"`
-	PhoneNumber string `gorm:"not null" json:"phone_number"`
-	Password string `gorm:"not null" json:"-"`
-	Role string `gorm:"not null" json:"role"`
+	FullName     string        `gorm:"not null" json:"full_name"`
+	Email        string        `gorm:"not null;unique" json:"email"`
+	PhoneNumber  string        `gorm:"not null" json:"phone_number"`
+	Password     string        `gorm:"not null" json:"-"`
+	Role         string        `gorm:"not null" json:"role"`
 	Reservations []Reservation `gorm:"foreignKey:UserID" json:"-"`
-	Reviews []Review `gorm:"foreignKey:UserID" json:"-"`
+	Reviews      []Review      `gorm:"foreignKey:UserID" json:"-"`
 }
