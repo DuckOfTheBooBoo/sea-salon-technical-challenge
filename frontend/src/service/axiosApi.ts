d52dd2628830785b284ac79f48f4a5cafa26477e
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-06-29 08:16:15 +0000

    feat(frontend/nav): Redirect to login page if user is unauthenticated

--- frontend/src/service/axiosApi.ts
+++ frontend/src/service/axiosApi.ts
@@ -1,4 +1,6 @@
-import axios, { type AxiosInstance } from 'axios';
+import axios, { type AxiosInstance, type AxiosResponse, type AxiosError } from 'axios';
+import * as constants from '@/constants';
+import router from '@/router';
 
 const apiClient: AxiosInstance = axios.create({
     baseURL: 'http://localhost:3000/api',
@@ -17,5 +19,17 @@ apiClient.interceptors.request.use((config) => {
     }
     return config;
 }, (error) => Promise.reject(error));
+
+apiClient.interceptors.response.use((response: AxiosResponse) => {
+    console.log(response)
+    return response;
+}, (error) => {
+    const err: AxiosError = error as AxiosError;
+    if (err.response?.status === constants.HTTP_UNAUTHORIZED) {
+        router.push({ name: 'login' });
+    }
+
+    return Promise.reject(error);
+});
 
 export default apiClient;
