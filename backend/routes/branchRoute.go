49a27320a0c0542fd52beb3476a044d1da1acab8
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-06-29 19:17:28 +0000

    feat(backend): Make branch get all api protected

--- backend/routes/branchRoute.go
+++ backend/routes/branchRoute.go
@@ -9,7 +9,7 @@ import (
 func BranchRoute(route *gin.RouterGroup) {
 	branches := route.Group("/branches")
 
-	branches.GET("", controllers.BranchGetAll)
+	branches.GET("", middlewares.JWTMiddleware(), controllers.BranchGetAll)
 	branches.POST("", middlewares.JWTMiddleware(), controllers.BranchCreate)
 	branches.PUT("/:id", middlewares.JWTMiddleware(), controllers.BranchUpdate)
 	branches.DELETE("/:id", middlewares.JWTMiddleware(), controllers.BranchDelete)
