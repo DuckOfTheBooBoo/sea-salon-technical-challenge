35d1b9997394bd57034aefd6fa7e08de8da3963c
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-07-02 08:27:50 +0000

    feat(backend): Add retry if db connection failed

--- backend/configs/database.go
+++ backend/configs/database.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -22,15 +24,39 @@ func (db *mysqlDB) GetDB() *gorm.DB {
 }
 
 func ConnectToDB() (Database, error) {
+	
 	dbName := os.Getenv("DB_NAME")
 	dbHost := os.Getenv("DB_HOST")
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASS")
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
-	gormDB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	retries := os.Getenv("MAX_RETRIES")
+
+	log.Println("DB_NAME:", os.Getenv("DB_NAME"))
+	log.Println("DB_HOST:", os.Getenv("DB_HOST"))
+	log.Println("DB_USER:", os.Getenv("DB_USER"))
+	log.Println("DB_PASS:", os.Getenv("DB_PASS"))
+	log.Println("MAX_RETRIES:", os.Getenv("MAX_RETRIES"))
+	
+	maxRetries, err := strconv.Atoi(retries)
 	if err != nil {
 		return nil, err
 	}
-	log.Println("Connected to database")
-	return &mysqlDB{db: gormDB}, nil
+
+	retryDelay := 5 * time.Second
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	
+	for attempts := 0; attempts < maxRetries; attempts++ {
+		gormDB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err == nil {
+			log.Println("Connected to database")
+			return &mysqlDB{db: gormDB}, nil
+		}
+
+		log.Printf("Failed to connect to database: %v. Retrying in %v...", err, retryDelay)
+		time.Sleep(retryDelay)
+	}
+
+	// Return the last error if all retries fail
+	return nil, fmt.Errorf("could not connect to database after %d attempts: %v", maxRetries, err)
 }
