56f442b997b7b3bbc94adc64c3dfe2ef610c55d7
Arajdian Altaf <74168834+DuckOfTheBooBoo@users.noreply.github.com>
2024-07-02 04:06:50 +0000

    feat(backend): Populate database on migration
    - Create branches
    - Create user admin
    - Create services
    - Create reviews

--- backend/configs/migration/migrate.go
+++ backend/configs/migration/migrate.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"log"
+	"math/rand"
+	"time"
 
 	"github.com/DuckOfTheBooBoo/sea-salon-technical-challenge/backend/configs"
 	"github.com/DuckOfTheBooBoo/sea-salon-technical-challenge/backend/models"
@@ -15,34 +17,197 @@ func init() {
 
 func createAdminUser(gormDB *gorm.DB) {
 	var user models.User
-	if err := gormDB.Where("email = ?", "thomas.n@compfest.id").First(&user).Error; err != nil {
-		if err == gorm.ErrRecordNotFound {
+	hashedPass, err := utils.HashPassword("Admin123")
 
-			hashedPass, err := utils.HashPassword("Admin123")
-
-			if err != nil {
-				log.Fatal(err.Error())
-				return
-			}
-
-			user = models.User{
-				FullName: "Thomas N",
-				Email:    "thomas.n@compfest.id",
-				Password: hashedPass,
-				Role:     "Admin",
-			}
-
-			if err := gormDB.Create(&user); err != nil {
-				log.Fatal(err)
-			}
-
-			log.Print("Admin user created successfully")
-		} else {
-			log.Fatal(err)
-		}
-	} else {
-		log.Print("Admin user already exists")
+	if err != nil {
+		log.Fatal(err.Error())
+		return
 	}
+
+	user = models.User{
+		FullName: "Thomas N",
+		Email:    "thomas.n@compfest.id",
+		Password: hashedPass,
+		Role:     "Admin",
+	}
+
+	if err := gormDB.Create(&user).Error; err != nil {
+		log.Println(err.Error())
+
+		if err == gorm.ErrDuplicatedKey {
+			log.Print("Admin user already exists")
+		}
+	}
+
+	log.Print("Admin user created successfully")
+}
+
+func createServices(gormDB *gorm.DB) {
+	var services []models.Service = []models.Service{
+		{
+			ServiceName: "Haircut",
+			ServiceCode: "haircut",
+			Duration:    30,
+		},
+		{
+			ServiceName: "Manicures & Pedicures",
+			ServiceCode: "manipedi",
+			Duration:    40,
+		},
+		{
+			ServiceName: "Facial Treatment",
+			ServiceCode: "facial",
+			Duration:    20,
+		},
+		{
+			ServiceName: "Nail Art",
+			ServiceCode: "nail-art",
+			Duration:    40,
+		},
+		{
+			ServiceName: "Scalp Treatments",
+			ServiceCode: "scalp-treat",
+			Duration:    35,
+		},
+	}
+
+	if err := gormDB.Create(&services).Error; err != nil {
+		log.Fatal(err.Error())
+	}
+
+	log.Print("Services created successfully")
+}
+
+func intTimeParser(s string) time.Time {
+	t, err := utils.ParseTime(s)
+
+	if err != nil {
+		log.Fatalf("Error parsing time: %v", err)
+	}
+
+	return t
+}
+
+func createBranches(gormDB *gorm.DB) {
+	var services []models.Service
+
+	if err := gormDB.Find(&services).Error; err != nil {
+		log.Fatal(err.Error())
+	}
+
+	var branches []models.Branch = []models.Branch{
+		{
+			BranchName: "SEA Salon - Pondok Indah Mall II",
+			BranchAddress: "Pondok Indah Mall 2, Level 1 Jl. Metro Pondok Indah Blok III-B Pondok Indah, Jakarta Selatan DKI Jakarta 12310 Indonesia",
+			Lat: -6.264692675855703,
+			Lng: 106.78453850564463,
+			OpenAt: intTimeParser("09:00"),
+			ClosedAt: intTimeParser("20:00"),
+		},
+		{
+			BranchName: "SEA Salon - Cilandak Town Square",
+			BranchAddress: "Jl. Cilandak No. 1, Cilandak, Jakarta Selatan 12430, Indonesia",
+			Lat: -6.291703706041596,
+			Lng: 106.79988239264421,
+			OpenAt: intTimeParser("10:00"),
+			ClosedAt: intTimeParser("21:00"),
+		},
+		{
+			BranchName: "SEA Salon - Plaza Blok M",
+			BranchAddress: "Plaza Bl. M, Blok M Plaza, Lantai 2, Jl. Bulungan No.76, RT.6/RW.6, Kramat Pela, Kebayoran Baru, South Jakarta City, Jakarta 12130",
+			Lat: -6.244134189991264,
+			Lng: 106.7975735709911,
+			OpenAt: intTimeParser("10:00"),
+			ClosedAt: intTimeParser("22:00"),
+		},
+	}
+
+	// Run the loop 3 times
+	for i := 0; i < 3; i++ {
+		numToPick := rand.Intn(2) + 2 // Generates either 2 or 3
+
+		// Slice to store the picked services
+		var pickedServices []models.Service
+
+		// Pick the services
+		for len(pickedServices) < numToPick {
+			index := rand.Intn(len(services)) // Generate a random index
+			pickedServices = append(pickedServices, services[index])
+		}
+
+		if err := gormDB.Create(&branches[i]).Error; err != nil {
+			log.Fatal(err.Error())
+		}
+
+		log.Print("Branch created successfully")
+
+		if err := gormDB.Model(&branches[i]).Association("Services").Append(&pickedServices); err != nil {
+			log.Fatal(err.Error())
+		}
+
+		log.Print("Branch services created successfully")
+	}
+}
+
+func createReviews(gormDB *gorm.DB) {
+	reviews := []models.Review{
+		{
+			FullName: "John Doe",
+			Rating:   5,
+			Comment:  "Great haircut, very satisfied!",
+		},
+		{
+			FullName: "Jane Smith",
+			Rating:   4,
+			Comment:  "Facial treatment was very relaxing.",
+		},
+		{
+			FullName: "Emily Davis",
+			Rating:   3,
+			Comment:  "Love my new nails!",
+		},
+		{
+			FullName: "Michael Johnson",
+			Rating:   5,
+			Comment:  "Manicure & pedicure were excellent.",
+		},
+		{
+			FullName: "Sarah Brown",
+			Rating:   4,
+			Comment:  "Scalp treatment was very soothing.",
+		},
+		{
+			FullName: "John Doe",
+			Rating:   5,
+			Comment:  "Excellent service and friendly staff.",
+		},
+		{
+			FullName: "Jane Smith",
+			Rating:   4,
+			Comment:  "Skin feels so smooth and rejuvenated.",
+		},
+		{
+			FullName: "Emily Davis",
+			Rating:   3,
+			Comment:  "Beautiful nail art, very creative.",
+		},
+		{
+			FullName: "Michael Johnson",
+			Rating:   5,
+			Comment:  "My nails look fantastic.",
+		},
+		{
+			FullName: "Sarah Brown",
+			Rating:   4,
+			Comment:  "Felt very relaxed after the treatment.",
+		},
+	}
+
+	if err := gormDB.Create(&reviews).Error; err != nil {
+		log.Fatal(err.Error())
+	}
+
+	log.Print("Reviews created successfully")
 }
 
 func main() {
@@ -56,10 +221,13 @@ func main() {
 
 	gormDB := db.GetDB()
 
-	if err := gormDB.AutoMigrate(&models.User{}, &models.Reservation{}, &models.Review{}, &models.Token{}, &models.Branch{}); err != nil {
+	if err := gormDB.AutoMigrate(models.Branch{}, models.Service{}, &models.User{}, &models.Reservation{}, &models.Review{}, &models.Token{}); err != nil {
 		log.Fatal(err.Error())
 		return
 	}
 
 	createAdminUser(gormDB)
+	createServices(gormDB)
+	createBranches(gormDB)
+	createReviews(gormDB)
 }
